d64795689758ebeb932500085cfdebb015908c09
gusgus <73510218+XpOriginal@users.noreply.github.com>
2025-06-13 14:13:11 -0300

    Add files via upload

--- Tp 7/Punto 3.asm
+++ Tp 7/Punto 3.asm
@@ -2,40 +2,59 @@
 	n: 	.word 5
 .text
 	main:
-		lw $s0, n
+		lw $t0, n			# $t0 = n
+		
+		addi $sp, $sp, -4		# Lugar para el parametro de salida
+		addi $sp, $sp, -4		# Lugar para el parametro de entrada
+		sw $t0, 0($sp)			# Guardamos $t0 en la pila (En el parametro de entrada)
+		
 		jal calcular
 		
+		lw $t4, 0($sp)			# Guardamos en $t4 lo que esta en el parametro de salida de la pila
+		addi $sp, $sp, 4		# Terminamos de borrar la lista
+		
 		li $v0, 1
-		move $a0, $v1
+		addi $a0, $t4, 0
 		syscall
 		
-	# Terminar el programa
-	li $v0, 10
-	syscall
-	
-	
-	calcular:
-		addi $sp, $sp, -4
-		sw $s0, 0($sp)
+		#Fin del programa
+		li $v0, 10
+		syscall
 		
-		andi $t0, $s0, 1	# Si el bit mas significativo es uno, es impar... si es 0 es par
-		beq $t0, $zero, esPar
-		j esImpar
+	calcular:
+		addi $sp, $sp, -4		# Espacio para la direccion de retorno
+		sw $ra, 0($sp)			# Cargamos direccion de retorno
+		addi $sp, $sp, -4		# Espacio para el frame pointer
+		sw $fp, 0($sp)			# Guardamos el frame pointer antiguo
+		addi $fp, $sp, 0		# Actualizamos el frame pointer
+		
+		lw $t3, 8($fp)			# Cargamos en t3 lo que esta 8 bits arriba del frame pointer ($t3 = Al primer parametro de entrada)
+		andi $t1, $t3, 1		# Si el bit mas significativo es 1 es impar, si no es par
+		beq $t1, $zero, esPar		# Chequea si es par o no es par, en caso de que sea salta hacia "esPar"
+		j esImpar			# En caso de que sea impar, salta a "esImpar"
+		
+		esPar:
+			srl $t3, $t3, 1		# $t3 = $t3/2
+			sw $t3, 12($fp)		# Guardamos al resultado en el parametro de salida
 			
-			esPar:
-				srl $v1, $s0, 1		# aux = n / 2	(Dos elevado a la una)
-				
-				lw $s0, 0($sp)
-				addi $sp, $sp, 4
-				
-				jr $ra
+			# Reset pila
+			addi $sp, $fp, 0	# Volvemos a $sp al $fp
+			lw $fp, 0($fp)		# Reiniciamos $fp, ahora apunta a otro lugar
+			addi $sp, $sp, 4	# Nos movemos 4 bits hacia arriba
+			lw $ra, 0($sp)		# Recuperamos la direccion de retorno
+			addi $sp, $sp, 8	# Saltamos el parametro de entrada quedandonos en el parametro de salida
 			
-			esImpar:
-				sll $v1, $s0, 2		# aux = n * 4	(Dos al cuadrado)
-				addi $v1, $v1, 1	# aux = aux + 1
-				
-				lw $s0, 0($sp)
-				addi $sp, $sp, 4
-				
-				jr $ra
-	
+			jr $ra
+		
+		esImpar:
+			sll $t3, $t3, 2
+			addi $t3, $t3, 1
+			sw $t3, 12($fp)		# Guardamos al resultado en el parametro de salida
+			
+			addi $sp, $fp, 0	# Volvemos a $sp al $fp
+			lw $fp, 0($fp)		# Reiniciamos $fp, ahora apunta a otro lugar
+			addi $sp, $sp, 4	# Nos movemos 4 bits hacia arriba
+			lw $ra, 0($sp)		# Recuperamos la direccion de retorno
+			addi $sp, $sp, 8	# Saltamos el parametro de entrada quedandonos en el parametro de salida
+			
+			jr $ra
