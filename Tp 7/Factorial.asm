9f3f01aea18624d26579cf8c10942123ca5ddae1
gusgus <73510218+XpOriginal@users.noreply.github.com>
2025-06-13 18:38:13 -0300

    Add files via upload

--- Tp 7/Factorial.asm
+++ Tp 7/Factorial.asm
@@ -10,9 +10,10 @@
 		
 		jal funcion			
 		
-		lw $t2, 0($sp)
-		add $sp, $sp, 4
+		lw $t2, 0($sp)			# Recuperamos el parametro de salida
+		add $sp, $sp, 4			# Resetamos la pila
 		
+		#Imprimimos el resultado
 		li $v0, 1
 		addi $a0, $t2, 0
 		syscall
@@ -30,31 +31,32 @@
 		
 		lw $t0, 8($fp)			# Cargamos en $t0 el parametro de entrada
 		
-		#Caso base
+		#Caso base	(USAMOS $t1 PARA EL PARAMETRO DE SALIDA)
 		bne $t0, $zero, recursion	# if $t0 != 0: Saltamos al caso recursivo
-		addi $t1, $zero, 1		# else: $t0 = 1
+		addi $t1, $zero, 1		# else: $t1 = 1
 		j retorno
 	
 	recursion:
-		addi $t0, $t0, -1
-		addi $sp, $sp, -4
-		addi $sp, $sp, -4
-		sw $t0, 0($sp)
+		addi $t0, $t0, -1		# (n-1)
+		addi $sp, $sp, -4		# Hacemos lugar para $t0
+		addi $sp, $sp, -4		# LINEA MAGICA, LA BORRAS Y MUERE EL CODIGO
+		sw $t0, 0($sp)			# Guardamos (n-1) en la pila
 		
-		jal funcion
+		jal funcion			# Recursion
 		
-		lw $t0, 0($sp)
-		addi $sp, $sp, 4
-		lw $t1, 8($fp)
+		#Se empieza a resolver la recursion
+		lw $t0, 0($sp)			# Recuperamos n-1 de la pila
+		addi $sp, $sp, 4		# "Borramos" n-1 de la pila
+		lw $t1, 8($fp)			# Recuperamos el parametro de entrada
 		
-		mul $t1, $t1, $t0
+		mul $t1, $t1, $t0		# n = n * f(n-1) (La cuenta del factorial)
 		
 	retorno:
-		sw $t1, 12($fp)
-		addi $sp, $fp, 0	# Volvemos a $sp al $fp
-		lw $fp, 0($fp)		# Reiniciamos $fp, ahora apunta a otro lugar
-		addi $sp, $sp, 4	# Nos movemos 4 bits hacia arriba
-		lw $ra, 0($sp)		# Recuperamos la direccion de retorno
-		addi $sp, $sp, 8	# Saltamos el parametro de entrada quedandonos en el parametro de salida
+		sw $t1, 12($fp)			# Guardamos el resultado del factorial en el parametro de salida 
+		addi $sp, $fp, 0		# Volvemos a $sp al $fp
+		lw $fp, 0($fp)			# Reiniciamos $fp, ahora apunta a otro lugar
+		addi $sp, $sp, 4		# Nos movemos 4 bits hacia arriba
+		lw $ra, 0($sp)			# Recuperamos la direccion de retorno
+		addi $sp, $sp, 8		# Saltamos el parametro de entrada quedandonos en el parametro de salida
 		jr $ra
 		
