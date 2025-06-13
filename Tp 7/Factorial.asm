a0f1047ff0d826bcbcf00d452fa3e937d01709e3
gusgus <73510218+XpOriginal@users.noreply.github.com>
2025-06-13 18:30:34 -0300

    Add files via upload

--- Tp 7/Factorial.asm
+++ Tp 7/Factorial.asm
@@ -1,44 +1,60 @@
 .data
-	n:		.word 5
-	resultado:	.word 0
+	n:		.word 7
 	
 .text
-	main:	
-		lw $a0, n		# Agarro el numero para el factorial (n!)
-		jal factorial		# Llamo a la funcion factorial
-		sw $v1, resultado	# resultado = factorial
+	main:
+		lw $t1, n			# $t1 = n
+		addi $sp, $sp, -4		# Lugar para el parametro de salida
+		addi $sp, $sp, -4		# Lugar para el parametro de entrada
+		sw $t1, 0($sp)			# Guardamos $t1 en la pila
+		
+		jal funcion			
+		
+		lw $t2, 0($sp)
+		add $sp, $sp, 4
 		
 		li $v0, 1
-		lw $a0, resultado	# Imprimimos el resultado
+		addi $a0, $t2, 0
 		syscall
 		
-	# Finalizamos el programa
-	li $v0, 10
-	addi $a0, $v0, 0
-	syscall
+		#Fin del programa
+		li $v0, 10
+		syscall
 	
-	factorial:
-		addi $sp, $sp, -8	# Iniciamos la pila
-		sw $s0, 0($sp)		# Cargamos s0 en la pila
-		sw $ra, 4($sp)
+	funcion:
+		addi $sp, $sp, -4		# Lugar para la direccion de retorno
+		sw $ra, 0($sp)			# Guardamos la direccion de retorno en la pila
+		addi $sp, $sp, -4		# Lugar para el frame pointer
+		sw $fp, 0($sp)			# Guardamos el frame pointer en la pila
+		move $fp, $sp			# Actualizamos el frame pointer
 		
-		# Caso base
-		li $v1, 1
-		beq $a0, $zero, factorialTerminado	# if (i == 0) -> return 1
+		lw $t0, 8($fp)			# Cargamos en $t0 el parametro de entrada
 		
-		# Else (i - 1)
-		move $s0, $a0		# s0 = a0(El numero ingresado)	| Con esta linea guardamos a nuestro numero a multiplicar en nuestra pila
-		sub $a0, $a0, 1		# (i - 1)
+		#Caso base
+		bne $t0, $zero, recursion	# if $t0 != 0: Saltamos al caso recursivo
+		addi $t1, $zero, 1		# else: $t0 = 1
+		j retorno
+	
+	recursion:
+		addi $t0, $t0, -1
+		addi $sp, $sp, -4
+		addi $sp, $sp, -4
+		sw $t0, 0($sp)
 		
-		jal factorial		# Recursividad
+		jal funcion
 		
-		# Multiplicamos nuestros numeros
-		mul $v1, $s0, $v1	# return = s0 * v1(El numero que devuelve la recursividad)
+		lw $t0, 0($sp)
+		addi $sp, $sp, 4
+		lw $t1, 8($fp)
 		
+		mul $t1, $t1, $t0
 		
-		factorialTerminado:
-			lw $s0, 0($sp)		# Recuperamos nuestro numero de la pila para futura ejecucion
-			lw $ra, 4($sp)
-			addi $sp, $sp, 8	# Reset pila
-			
-			jr $ra			# Fin de la funcion (Volvemos al main)
+	retorno:
+		sw $t1, 12($fp)
+		addi $sp, $fp, 0	# Volvemos a $sp al $fp
+		lw $fp, 0($fp)		# Reiniciamos $fp, ahora apunta a otro lugar
+		addi $sp, $sp, 4	# Nos movemos 4 bits hacia arriba
+		lw $ra, 0($sp)		# Recuperamos la direccion de retorno
+		addi $sp, $sp, 8	# Saltamos el parametro de entrada quedandonos en el parametro de salida
+		jr $ra
+		
